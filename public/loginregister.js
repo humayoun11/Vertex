83738b54ae691df2ed2fbc4328e4c0e53da55699
humayoun11 <humayounkhatri@gmail.com>
2023-02-16 00:09:16 +0500

    update

--- public/loginregister.js
+++ public/loginregister.js
@@ -36,24 +36,44 @@ window.onclick = function (event) {
     loginpage.style.display = 'none'
   }
 }
-function registration() {
-  var username = document.getElementById('RUser').value
-  var email = document.getElementById('REmail').value
-  var password = document.getElementById('RPassword').value
-  var number = document.getElementById('RNumber').value
 
-  localStorage.setItem('notapprovedUsername', username)
-  localStorage.setItem('notapprovedUserEmail', email)
-  localStorage.setItem('notapprovedUserPassword', password)
-  localStorage.setItem('notapprovedUsernumber', number)
-  if (username.length >= 6 && email.length >= 6 && password.length >= 6) {
+// function registrationaddsave() {
+//   registration()
+//   add_text_input()
+//   savedata()
+// }
+function registration() {
+  let username = document.getElementById('RUser').value,
+    email = document.getElementById('REmail').value,
+    password = document.getElementById('RPassword').value,
+    number = document.getElementById('RNumber').value
+
+  let formData = JSON.parse(localStorage.getItem('formData')) || []
+
+  let exist =
+    formData.length &&
+    JSON.parse(localStorage.getItem('formData')).some(
+      (data) =>
+        data.username.toLowerCase() == username.toLowerCase() &&
+        data.email.toLowerCase() == email.toLowerCase() &&
+        data.number.toLowerCase() == number.toLowerCase(),
+    )
+
+  if (!exist) {
+    formData.push({ username, email, password, number })
+    localStorage.setItem('formData', JSON.stringify(formData))
+    document.querySelector('form').reset()
+    document.getElementById('RUser').focus()
     alert('registration successfull')(
       (window.location.href = 'loginsignup.html'),
     )
   } else {
-    alert('Please Fill Complete form')
+    alert('Ooopppssss... Duplicate found!!!\nYou have already sigjned up')
   }
+  e.preventDefault()
+  
 }
+
 function login() {
   var adminuser = 'admin'
   var adminpassword = 'admin123'
@@ -68,10 +88,10 @@ function login() {
   var notapprovedusername = localStorage.getItem('notapprovedUsername')
   var notapprovedUserPassword = localStorage.getItem('notapprovedUserPassword')
 
-  if (user == localUser && password == localPass) {
-    alert('login successfully ' + localStorage.localUsername)
-    ;(window.location.href = 'userhome.html'), (showuser.innerHTML = localUser)
-  }
+  // if (user == localUser && password == localPass) {
+  //   alert('login successfully ' + localStorage.localUsername)
+  //   ;(window.location.href = 'userhome.html'), (showuser.innerHTML = localUser)
+  // }
 
   //   document.getElementById('showusername').innerHTML = localUser
   if (user == notapprovedusername && password == notapprovedUserPassword) {
@@ -94,4 +114,46 @@ function login() {
     alert('kindly pleasee correct your credentials')
   }
 }
+function add_text_input() {
+  var table = document.getElementById('mytable')
+  var x = table.rows.length
+  table.insertRow(-1).innerHTML =
+    '<tr>' +
+    '<td> <input type="text" id="username' +
+    x +
+    '" /></td>' +
+    '<td> <input type="text" id="email' +
+    x +
+    '" /></td>' +
+    '<td> <input type="text" id="number' +
+    x +
+    '" /></td></tr>'
+}
+function save_data() {
+  var table = document.getElementById('mytable')
+  var tableRows = table.rows.length
+  var data = []
+  for (var i = 1; i <= tableRows - 1; i++) {
+    var title = document.getElementById('username' + i).value
+    var desc = document.getElementById('useremail' + i).value
+    var desc = document.getElementById('usernumber' + i).value
+    var desc = document.getElementById('userpassword' + i).value
+    var temp = { title: title, description: desc }
+    data.push(temp)
+  }
+  window.localStorage.setItem('UserDeatil', JSON.stringify(data))
+}
+loadData = function () {
+  let data = JSON.parse(window.localStorage.getItem('Table1'))
+  for (i = 0; i < data.length; i++) {
+    add_text_input()
+    document.getElementById('username' + (i + 1)).value = data[i].username
+    document.getElementById('email' + (i + 1)).value = data[i].email
+    document.getElementById('password' + (i + 1)).value = data[i].password
+    document.getElementById('number' + (i + 1)).value = data[i].number
+  }
+}
+
+loadData()
+
 var showuser = (document.getElementById('showusername').innerHTML = localUser)
