38746b3592d76641ca62c27dc119205d6656f7be
humayoun11 <humayounkhatri@gmail.com>
2023-02-21 02:33:53 +0500

    update

--- public/commentfeedback.js
+++ public/commentfeedback.js
@@ -1,5 +1,28 @@
 function submit() {
-  var comment = document.getElementById('commenttext').value
+  let uvirusname = document.getElementById('virus').value,
+    userdetailname = localStorage.getItem('localUsername'),
+    commenttext = document.getElementById('commenttext').value
 
-  localStorage.setItem('localcomment', comment)
+  var comment = JSON.parse(localStorage.getItem('comment')) || []
+
+  let exist =
+    comment.length &&
+    JSON.parse(localStorage.getItem('comment')).some(
+      (data) =>
+        data.id &&
+        data.uvirusname.toLowerCase() == uvirusname.toLowerCase() &&
+        data.userdetailname.toLowerCase() == userdetailname.toLowerCase() &&
+        data.commenttext.toLowerCase() == commenttext.toLowerCase(),
+    )
+
+  if (!exist) {
+    comment.push({ uvirusname, userdetailname, commenttext })
+    localStorage.setItem('comment', JSON.stringify(comment))
+
+    alert('comment successfull')
+    // ((window.location.href = 'loginsignup.html'),)
+  } else {
+    alert('Ooopppssss... Duplicate found!!!\nYou have already sigjned up')
+  }
+  e.preventDefault()
 }
